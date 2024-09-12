fef0ec33f7c7eb2910666f4c41bdb61f93f19c09
hamsasaber <hamsa_saber_03@hotmail.com>
2024-09-12 20:58:56 +0300

    homepage

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,11 +1,12 @@
 "use client";
 import NavBar from "@/components/NavBar";
 import React, { useState, useEffect } from "react";
-import { FaFacebook, FaTwitter } from "react-icons/fa";
-import { MdOutlineMail } from "react-icons/md";
+import { FaFacebook, FaTwitter, FaArrowRight } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import axios from "axios";
+import BookCard from "@/components/BookCard";
+import Footer from "@/components/Footer";
 
 const Page = () => {
   const [footer, setFooter] = useState(false);
@@ -13,18 +14,25 @@ const Page = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    axios.get("http://192.168.1.31:9850/api/v1/books/").then((res) => {
-      setBooks(res.data);
-    });
+    axios
+      .get("http://localhost:9850/api/v1/books/")
+      .then((res) => {
+        const trendingBooks = res.data.filter((book) => book.nbRead > 5);
+        setBooks(trendingBooks);
+      })
+      .catch((err) => {
+        console.error("Error fetching books:", err);
+      });
   }, []);
 
-  console.log(books);
   return (
-    <div className="bg-[#feede5] h-screen py-2">
+    <div className="bg-[#feede5] py-2">
       <NavBar setFooter={setFooter} footer={footer} />
       <div>
         <div className="flex items-start bg-[#f4acb7] rounded flex-col shadow-lg m-2 w-1/2">
-          <div className="text-2xl font-bold text-[#290d11] p-2">Booked Up</div>
+          <div className="text-2xl font-bold text-[#290d11] p-2 border-4 border-white">
+            Booked Up
+          </div>
           <div className="space-y-3 text-white p-2">
             <p className="leading-relaxed">
               BookedUp is your go-to platform for connecting authors and readers
@@ -47,77 +55,28 @@ const Page = () => {
         </div>
       </div>
 
-      <div>
-        <div>Trending Right Now</div>
-        {books.length > 0 ? (
-          books.map((book) => <div key={book.id}>{book.title}</div>)
-        ) : (
-          <div>No books available.</div>
-        )}
-      </div>
-      {footer && (
-        <div className="fixed bottom-0 bg-[#BDE0FE] rounded p-4 shadow-lg w-screen flex flex-row justify-between items-center">
-          <div className="flex flex-col items-start">
-            <div className="text-xl font-semibold text-gray-900 mb-4">Info</div>
-            <div className="space-y-3">
-              <p className="leading-relaxed">
-                Welcome to{" "}
-                <span className="font-bold text-red-600">BookedUp</span>! Our
-                web application allows authors to publish their books,
-              </p>
-              <p className="leading-relaxed">
-                and readers to easily browse, purchase, or rent them.
-              </p>
-              <p className="leading-relaxed">
-                Authors can upload their books with cover images and
-                descriptions,
-              </p>
-              <p className="leading-relaxed">
-                setting their prices for sale or rent. Readers can explore the
-                available titles,
-              </p>
-              <p className="leading-relaxed">
-                and complete their purchases or rentals after entering their
-                credit card details and becoming a customer!
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col items-end">
-            <div className="mt-2 mb-4 font-semibold">Contact Us</div>
-            <div className="flex flex-row gap-8">
-              <button
-                onClick={() => {
-                  router.push(
-                    "https://www.facebook.com/profile.php?id=61565194411026"
-                  );
-                }}
-              >
-                <FaFacebook
-                  size={32}
-                  className="text-[#1877F2] hover:text-white"
-                />
-                <span className="text-xs text-[#290d11] mt-1">Facebook</span>
-              </button>
-
-              <button>
-                <FaTwitter
-                  size={32}
-                  className="text-[#1DA1F2] hover:text-white"
-                />
-                <span className="text-xs text-[#290d11] mt-1">Twitter</span>
-              </button>
-
-              <button>
-                <MdOutlineMail
-                  size={32}
-                  className="text-[#D44638] hover:text-white"
-                />
-                <span className="text-xs text-[#290d11] mt-1">Email</span>
-              </button>
-            </div>
-          </div>
+      <div className="mt-20 ml-2 w-60 h-30 shadow-xl">
+        <div className="bg-[#caf0f8] text-2xl text-[#290d11] font-bold border-4 border-[#290d11]">
+          Trending Right Now
         </div>
-      )}
+      </div>
+      <div className="mt-2 ml-5 mr-5 bg-[#f5cac3] p-4 rounded shadow-md">
+        <BookCard books={books} setBooks={setBooks} />
+        <div className="text-lg text-[#290d11] font-semibold">
+          {" "}
+          Cant Find what you're looking for? <br />{" "}
+          <button
+            className="flex flex-row gap-2 text-[#290d11]"
+            onClick={() => {
+              router.push("/exploreMore");
+            }}
+          >
+            Explore more <FaArrowRight color="#290d11" />
+          </button>
+        </div>
+      </div>
+
+      {footer && <Footer />}
     </div>
   );
 };
